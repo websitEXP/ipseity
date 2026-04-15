807f00b7bf5a76605976f6e80504f12e43a63214
websitEXP <jamen96903@gmail.com>
2026-04-15 19:43:40 +0100

    Update app.tsx
    the 404 erur corection

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -22,7 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <SeasonProvider>
-        <BrowserRouter>
+        <HashRouter>
           <div className="grain-overlay season-transition min-h-screen bg-forest">
             <ParticleEffect />
             <Header />
@@ -37,7 +37,7 @@ const App = () => (
             <Footer />
             <CatMascot />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </SeasonProvider>
     </TooltipProvider>
   </QueryClientProvider>
